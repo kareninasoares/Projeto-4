1090e901eb84ca0c4b949842646a6d5b9fd5edb0
kareninasoares <102702607+kareninasoares@users.noreply.github.com>
2022-05-06 22:27:42 -0300

    Add files via upload

---
+++ Atv 2.py
@@ -0,0 +1,11 @@
+import os
+n1 = float(input('Informe a nota 1: '))
+n2 = float(input('Informe a nota 2: '))
+n3 = float(input('Informe a nota 3: '))
+n4 = float(input('Informe a nota 4: '))
+média = (n1+n2+n3+n4)/4
+txt = ('A média é de {}')
+print(txt . format(média))
+minhalista = [10,7,9,5]
+minhalista.sort()
+print(minhalista)
